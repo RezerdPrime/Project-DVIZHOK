5a464e62ac747d558031c4c716b3871f2d8a071c
Oswald Steinhelß <101434848+RezerdPrime@users.noreply.github.com>
2023-06-15 23:10:22 +1000

    Update Movement.py

--- Engine/Movement.py
+++ Engine/Movement.py
@@ -2,8 +2,13 @@ import keyboard
 import pyautogui as pag
 from Engine import *
 
+config = configparser.ConfigParser()
+config.read("config.cfg")
+speed_cfg = float(config['MANAGING']['speed'])
+dd_diff = abs(float(config['MANAGING']['draw_dist']))
+sens = float(config['MANAGING']['sensitivity'])
 
-def move_forward(cam: Camera, speed=1):
+def move_forward(cam: Camera, speed=speed_cfg):
     if cam.look_dir != Vector(0, 1, 0) \
             and cam.look_dir != Vector(0, -1, 0):
         y = cam.look_dir.pt.c2
@@ -13,11 +18,11 @@ def move_forward(cam: Camera, speed=1):
         cam.screen.pos = cam.pos + cam.look_dir.pt
     else:
         cam.pos = cam.pos - cam.screen.u.pt * speed
-        cam.screen.pos = cam.pos + cam.look_dir.p
+        cam.screen.pos = cam.pos + cam.look_dir.pt
 
     return cam
 
-def move_backward(cam: Camera, speed=1):
+def move_backward(cam: Camera, speed=speed_cfg):
     if cam.look_dir != Vector(0, 1, 0) \
             and cam.look_dir != Vector(0, -1, 0):
         y = cam.look_dir.pt.c2
@@ -31,7 +36,7 @@ def move_backward(cam: Camera, speed=1):
 
     return cam
 
-def move_left(cam: Camera, speed=1):
+def move_left(cam: Camera, speed=speed_cfg):
     y = cam.screen.v.pt.c2
     cam.screen.v.pt.c2 = 0
     cam.pos = cam.pos - cam.screen.u.pt * speed
@@ -39,24 +44,31 @@ def move_left(cam: Camera, speed=1):
     cam.screen.v.pt.c2 = y
     return cam
 
-def move_right(cam: Camera, speed=1):
+def move_right(cam: Camera, speed=speed_cfg):
     return move_left(cam, -speed)
 
-def move_to_viewpoint(cam: Camera, speed=1):
-    cam.pos = cam.pos + cam.look_dir.pt * speed
+def move_up(cam: Camera, speed=speed_cfg):
+    cam.pos = cam.pos + Point(0, speed, 0) #cam.look_dir.pt * speed
+    cam.screen.pos = cam.screen.pos + Point(0, speed, 0)
+    cam.look_dir = cam.look_dir + Vector(0, speed, 0)
     return cam
 
 
-def move_from_viewpoint(cam: Camera, speed=1):
-    cam.pos = cam.pos - cam.look_dir.pt * speed
+def move_down(cam: Camera, speed=speed_cfg):
+    cam.pos = cam.pos - Point(0, speed, 0) #cam.look_dir.pt * speed
+    cam.screen.pos = cam.screen.pos - Point(0, speed, 0)
+    cam.look_dir = cam.look_dir - Vector(0, speed, 0)
     return cam
 
-def draw_dist_up(cam: Camera, val=5):
+def draw_dist_up(cam: Camera, val=dd_diff):
     cam.draw_dist += val
+    if cam.draw_dist > 250: cam.draw_dist = 250
     return cam
 
-def draw_dist_down(cam: Camera, val=5):
-    return draw_dist_up(cam, -val)
+def draw_dist_down(cam: Camera, val=dd_diff):
+    cam.draw_dist -= val
+    if cam.draw_dist < 0: cam.draw_dist = 0
+    return cam
 
 
 Events.add("w")
@@ -74,13 +86,13 @@ class Spectator(Camera):
     Events.add('shift + s')
     Events.add('shift + z')
     Events.add('shift + x')
-    Events.handle('shift + w', move_to_viewpoint)
-    Events.handle('shift + s', move_from_viewpoint)
+    Events.handle('shift + w', move_up)
+    Events.handle('shift + s', move_down)
     Events.handle('shift + z', draw_dist_up)
     Events.handle('shift + x', draw_dist_down)
 
 #class Player(Camera):
-def launch(console: Console, camera_type: str = 'spectator', sensitivity=2, move_speed=1):
+def launch(console: Console, camera_type: str = 'spectator', sensitivity=sens, speed=speed_cfg):
 
     #global exit_cond
     exit_cond = True
@@ -96,7 +108,7 @@ def launch(console: Console, camera_type: str = 'spectator', sensitivity=2, move
             print("Exit: Successful")
 
         def act(action: str):
-            console.cam = Events.trigger(action, console.cam, move_speed)
+            console.cam = Events.trigger(action, console.cam, speed)
             console.draw()
 
         keyboard.add_hotkey('ctrl+q', close_console)
@@ -120,7 +132,7 @@ def launch(console: Console, camera_type: str = 'spectator', sensitivity=2, move
 
             if new_pos != curr_pos:
                 something_happened = True
-                difference = [(new_pos[0] - curr_pos[0]) * sensitivity, (new_pos[1] - curr_pos[1]) * sensitivity / 3]
+                difference = [(new_pos[0] - curr_pos[0]) * sensitivity, (new_pos[1] - curr_pos[1]) * sensitivity / 5]
                 difference[0] /= (pag.size()[0] // 2)
                 difference[1] /= (pag.size()[1] // 2)
                 t, s = difference
